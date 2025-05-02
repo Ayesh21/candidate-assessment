5ac567d732f371f30ee898e10e9b0168c156474e
Ayesh Navindu <navindu35@gmail.com>
2025-05-02 20:24:48 +0530

    Initial commit

---
+++ src/main/java/com/teleport/candidate_assessment/CandidateAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.teleport.candidate_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateAssessmentApplication.class, args);
+	}
+
+}
